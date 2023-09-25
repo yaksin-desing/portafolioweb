bc16013a65ae6c9bbf4916b44abe7900a16276fd
yaksin <yaksinvasquez@gmail.com>
2023-09-25 18:25:57 -0500

    modificacion de estilo

--- js/script.js
+++ js/script.js
@@ -35,7 +35,6 @@ cursorScale.forEach(link => {
     });
 });
 
-
 LottieInteractivity.create({
     mode: "chain",
     player: "#logo",
@@ -68,6 +67,9 @@ LottieInteractivity.create({
     ]
 });
 
+
+//MENU// 
+
 const iconoMenu = document.querySelector('.icono-menu'),
     menu = document.querySelector('.cont-menu');
 iconoMenu.addEventListener('click', (e) => {
@@ -76,116 +78,13 @@ iconoMenu.addEventListener('click', (e) => {
     const rutaActual = e.target.getAttribute('src');
     if (rutaActual == '../assets/img/menu1.svg' || rutaActual == '../assets/img/menu1.svg') {
         e.target.setAttribute('src', '../assets/img/menu2.svg');
-
     } else {
         e.target.setAttribute('src', '../assets/img/menu1.svg');
-
     }
 });
 
-// animacion barba.js//
-
-pageTransition = () => {
-    var timeline = gsap.timeline();
-
-    timeline.to("header", {
-        zIndex: 1
-    });
-
-    timeline.to(".page-transition", {
-        duration: .2,
-        height: "120%",
-        button: "100%",
-
-    });
-    timeline.to(".pages-transition", {
-        duration: .2,
-        height: "120%",
-        button: "100%",
-    });
-    timeline.to(".pages-transition", {
-        duration: .3,
-        height: "0%",
-        button: "0%",
-        delay: .1,
-        yoyo: true
-
-    });
-    timeline.to(".page-transition", {
-        duration: .4,
-        height: "0%",
-        button: "0%",
-        delay: .1,
-        yoyo: true
-
-    });
-
-    timeline.set(".page-transition", {
-        button: "-100%"
-    });
-}
-
-mainAnimation = () => {
-    var timeline = gsap.timeline();
-
-    timeline.from(".container", {
-        duration: 1,
-        y: 30,
-        opacity: 0,
-        stagger: {
-            amount: .4
-        },
-        delay: .8
-    });
-}
-
-delay = (n) => {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    })
-}
 
 
-
-// barba.init({
-//     sync: true,
-//     transitions: [{
-//         async leave(data) {
-//             const done = this.async();
-//             pageTransition();
-//             await delay(1000);
-//             done();
-//         },
-//         async enter(data) {
-//             mainAnimation();
-//         },
-//         async once(data) {
-//             mainAnimation();
-//         }
-//     }],
-// });
-
-// despues de transicion
-barba.hooks.afterLeave((data) => {
-    let triggers = ScrollTrigger.getAll();
-    triggers.forEach(trigger => {
-        trigger.kill();
-    });
-});
-// restablecer despues de transicion
-barba.hooks.enter((data) => {
-    window.scrollTo(0, 0);
-    ScrollTrigger.refresh(true);
-});
-// reiniciar funciones despues de transicion
-barba.hooks.afterEnter((data) => {
-    // console.log(data.next.namespace);
-    // var x = data.next.namespace;
-    reInitialized();
-});
 
 
 let valor = 0;
