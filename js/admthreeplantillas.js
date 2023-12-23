65cde0a2f8ec779872cad144fe452191588a9f00
yaksin <yaksinvasquez@gmail.com>
2023-12-23 01:36:43 -0500

    add animate figures in the section inicio

--- js/admthreeplantillas.js
+++ js/admthreeplantillas.js
@@ -1,27 +1,9 @@
-gsap.registerPlugin(ScrollTrigger)
-
-var tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".cont_inicio",
-        pin: true,
-        star: "top top",
-        end: "100% 100%",
-        scrub: 9,
-        // markers: {
-        //     startColor: "black",
-        //     endColor: "blue",
-        //     fontSize: "18px",
-        //     fontWeight: "bold",
-        //     indent: 20
-        // }
-    },
-})
 
 
 const figurauxui = document.querySelector("div.columdos")
 
 const sceneuxui = new THREE.Scene();
-const camerauxui = new THREE.PerspectiveCamera(70, figurauxui.clientWidth / figurauxui.clientHeight, 1, 0);
+const camerauxui = new THREE.PerspectiveCamera(60, figurauxui.clientWidth / figurauxui.clientHeight, 1, 0);
 
 const rendereruxui = new THREE.WebGLRenderer({
     alpha: true,
@@ -40,26 +22,21 @@ const materialuxui = new THREE.MeshPhysicalMaterial({
     roughness: 0.6,
     transmission: 0.9,
     thickness: 2,
-
 });
-
 const materialuxuidos = new THREE.MeshPhysicalMaterial({
-    wireframe:true,
+    wireframe:false,
     roughness: 0.5,
     transmission: 0.8,
     thickness: 1,
 
 });
 
-
-
-
 const vidriouxui = new THREE.Mesh(geometryuxui, materialuxui);
 vidriouxui.receiveShadow = true;
 vidriouxui.castShadow = true;
 sceneuxui.add(vidriouxui);
 
-vidriouxui.position.x = -2;
+vidriouxui.position.x = 0.6;
 vidriouxui.position.z = 1;
 camerauxui.position.z = 10;
 
@@ -70,7 +47,7 @@ vidriouxuidos.receiveShadow = true;
 vidriouxuidos.castShadow = true;
 sceneuxui.add(vidriouxuidos);
 
-vidriouxuidos.position.x = -1.7;
+vidriouxuidos.position.x = 1.1;
 vidriouxuidos.position.z = 0;
 
 // ############### 3. vidrio#############
@@ -80,7 +57,7 @@ vidriouxuitres.receiveShadow = true;
 vidriouxuitres.castShadow = true;
 sceneuxui.add(vidriouxuitres);
 
-vidriouxuitres.position.x = -1.1;
+vidriouxuitres.position.x = 1.4;
 vidriouxuitres.position.z = 1;
 
 
@@ -91,7 +68,7 @@ vidriouxuicuatro.receiveShadow = true;
 vidriouxuicuatro.castShadow = true;
 sceneuxui.add(vidriouxuicuatro);
 
-vidriouxuicuatro.position.x = -0.7;
+vidriouxuicuatro.position.x = 2;
 vidriouxuicuatro.position.z = 0;
 
 // ############### 5. vidrio#############
@@ -101,7 +78,7 @@ vidriouxuicinco.receiveShadow = true;
 vidriouxuicinco.castShadow = true;
 sceneuxui.add(vidriouxuicinco);
 
-vidriouxuicinco.position.x = -0.2;
+vidriouxuicinco.position.x = 2.2;
 vidriouxuicinco.position.z = 1;
 
 // ############### 6. vidrio#############
@@ -111,7 +88,7 @@ vidriouxuiseis.receiveShadow = true;
 vidriouxuiseis.castShadow = true;
 sceneuxui.add(vidriouxuiseis);
 
-vidriouxuiseis.position.x = 0.2;
+vidriouxuiseis.position.x = 2.9;
 vidriouxuiseis.position.z = 0;
 
 // ############### 7. vidrio#############
@@ -121,7 +98,7 @@ vidriouxuiciete.receiveShadow = true;
 vidriouxuiciete.castShadow = true;
 sceneuxui.add(vidriouxuiciete);
 
-vidriouxuiciete.position.x = 0.7;
+vidriouxuiciete.position.x = 3.3;
 vidriouxuiciete.position.z = 0;
 
 // ############### 8. vidrio#############
@@ -131,7 +108,7 @@ vidriouxuiocho.receiveShadow = true;
 vidriouxuiocho.castShadow = true;
 sceneuxui.add(vidriouxuiocho);
 
-vidriouxuiocho.position.x = 1.2;
+vidriouxuiocho.position.x = 3.7;
 vidriouxuiocho.position.z = 0;
 
 // ############### 9. vidrio#############
@@ -141,7 +118,7 @@ vidriouxuinueve.receiveShadow = true;
 vidriouxuinueve.castShadow = true;
 sceneuxui.add(vidriouxuinueve);
 
-vidriouxuinueve.position.x = 3;
+vidriouxuinueve.position.x = 4.1;
 vidriouxuinueve.position.z = 0;
 
 // ############### 10. vidrio#############
@@ -151,7 +128,7 @@ vidriouxuidies.receiveShadow = true;
 vidriouxuidies.castShadow = true;
 sceneuxui.add(vidriouxuidies);
 
-vidriouxuidies.position.x = 3.4;
+vidriouxuidies.position.x = 4.5;
 vidriouxuidies.position.z = 0;
 
 
@@ -162,7 +139,7 @@ vidriouxuionce.receiveShadow = true;
 vidriouxuionce.castShadow = true;
 sceneuxui.add(vidriouxuionce);
 
-vidriouxuionce.position.x = 3.8;
+vidriouxuionce.position.x = 4.9;
 vidriouxuionce.position.z = 0;
 
 
@@ -174,7 +151,7 @@ vidriouxuidoce.receiveShadow = true;
 vidriouxuidoce.castShadow = true;
 sceneuxui.add(vidriouxuidoce);
 
-vidriouxuidoce.position.x = 4.2;
+vidriouxuidoce.position.x = 5.3;
 vidriouxuidoce.position.z = 0;
 
 
@@ -186,7 +163,7 @@ vidriouxuitrece.receiveShadow = true;
 vidriouxuitrece.castShadow = true;
 sceneuxui.add(vidriouxuitrece);
 
-vidriouxuitrece.position.x = 5.6;
+vidriouxuitrece.position.x = 5.7;
 vidriouxuitrece.position.z = 0;
 
 
@@ -198,7 +175,7 @@ vidriouxuicatorce.receiveShadow = true;
 vidriouxuicatorce.castShadow = true;
 sceneuxui.add(vidriouxuicatorce);
 
-vidriouxuicatorce.position.x = 6;
+vidriouxuicatorce.position.x = 6.1;
 vidriouxuicatorce.position.z = 0;
 
 // ############### 15. vidrio#############
@@ -212,36 +189,9 @@ vidriouxuiquince.position.x = 6.4;
 vidriouxuiquince.position.z = 0;
 
 
-// ############### 16. vidrio#############
-
-const vidriouxuidiesiseis = new THREE.Mesh(geometryuxui, materialuxuidos);
-vidriouxuidiesiseis.receiveShadow = true;
-vidriouxuidiesiseis.castShadow = true;
-sceneuxui.add(vidriouxuidiesiseis);
-
-vidriouxuidiesiseis.position.x = 6.7;
-vidriouxuidiesiseis.position.z = 0;
-
-// ############### 17. vidrio#############
-
-const vidriouxuidiesisiete = new THREE.Mesh(geometryuxui, materialuxui);
-vidriouxuidiesisiete.receiveShadow = true;
-vidriouxuidiesisiete.castShadow = true;
-sceneuxui.add(vidriouxuidiesisiete);
-
-vidriouxuidiesisiete.position.x = 7;
-vidriouxuidiesisiete.position.z = 0;
 
 
-// ############### 18. vidrio#############
 
-const vidriouxuidiesiocho = new THREE.Mesh(geometryuxui, materialuxuidos);
-vidriouxuidiesiocho.receiveShadow = true;
-vidriouxuidiesiocho.castShadow = true;
-sceneuxui.add(vidriouxuidiesiocho);
-
-vidriouxuidiesiocho.position.x = 7.3;
-vidriouxuidiesiocho.position.z = 0;
 
 
 
@@ -257,7 +207,7 @@ const figurados = new THREE.Mesh(geometrydos, materialdos);
 figurados.receiveShadow = true;
 sceneuxui.add(figurados);
 
-figurados.position.x = -5;
+figurados.position.x = -13;
 figurados.position.z = -4;
 
 
@@ -270,78 +220,50 @@ const figuratres = new THREE.Mesh(geometrytres, materialdos);
 figuratres.receiveShadow = true;
 sceneuxui.add(figuratres);
 
-figuratres.position.x = -15;
+figuratres.position.x = -13;
 figuratres.position.z = -4;
 
 
 // ###############figuracuatro#############
 
-const geometrycuatro = new THREE.IcosahedronGeometry(3);
+const geometrycuatro = new THREE.IcosahedronGeometry(3,0);
 
 
 const figuracuatro = new THREE.Mesh(geometrycuatro, materialdos);
 figuracuatro.receiveShadow = true;
 sceneuxui.add(figuracuatro);
 
-figuracuatro.position.x = 15;
-figuracuatro.position.z = 2;
-
-
-const clock = new THREE.Clock()
-let v = new THREE.Vector3();
-let pos = vidriouxui.geometry.attributes.position;
-
-
-const t = clock.getElapsedTime()
-for (let i = 0; i < pos.count; i++) {
-    v.fromBufferAttribute(pos, i);
-    const wavex1 = 0.05 * Math.sin(20 * v.x + t * 1)
-    const wavex2 = 0.05 * Math.sin(20 * v.x + t * 1)
-    pos.setZ(i, wavex1 + wavex2)
-}
-pos.needsUpdate = true;
-
-
+figuracuatro.position.x = -13;
+figuracuatro.position.z = -3;
 
 function animate() {
-
-
     figurados.rotation.x += -0.005;
     figurados.rotation.y += 0.005;
     figuratres.rotation.x += -0.01;
     figuratres.rotation.y += 0.01;
-
     figuracuatro.rotation.x += -0.01;
     figuracuatro.rotation.y += 0.01;
-    //figurados.position.x += 0.03;
-
-
 
     requestAnimationFrame(animate);
     rendereruxui.render(sceneuxui, camerauxui);
 }
 animate();
 
-tl.to(
-    figurados.position,{
-        x:10,
-        duration: 5,
+var rebo = gsap.timeline({repeat: -1, repeatDelay:0 });
+rebo.to(figurados.position, {
+    x:13,
+    duration: 8,
+    ease: "power1.in",    
+});
 
-    },
-)
+rebo.to(figuratres.position, {
+    x:13,
+    duration: 8,
+    ease: "power1.in",
+});
 
-tl.to(
-    figuratres.position, {
-        x:-10,
-        duration: 1,
-
-    },
-)
-
-tl.to(
-    figuracuatro.position, {
-        x:0,
-        duration: 5,
-
-    },
-)
+rebo.to(figuracuatro.position, {
+    x:13,
+    duration: 8,
+    ease: "power1.in",
+});
