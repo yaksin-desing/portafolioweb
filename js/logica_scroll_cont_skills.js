ba5dfbb596d441d69ca7ac6030de0b3a3de007b0
yaksin <yaksinvasquez@gmail.com>
2024-05-28 01:33:42 -0500

    add animacion de scroll

---
+++ js/logica_scroll_cont_skills.js
@@ -0,0 +1,31 @@
+const skillss = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".cont_animacionmiskills_vertical",
+        start: 'top top',
+        end: '700% top',
+        markers: true,
+        pin: true,
+        scrub: 1,
+    }
+});
+
+skillss.to("#uno", {
+    y: "00vh",
+    duration: 100,
+});
+
+skillss.to("#dos", {
+    y: "00vh",
+    duration: 100,
+});
+
+
+skillss.to("#tres", {
+    y: "00vh",
+    duration: 100,
+});
+
+skillss.to("#cuatro", {
+    y: "00vh",
+    duration: 100,
+});
