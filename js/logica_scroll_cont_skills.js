d9f340687e8b75999c5d9d442ebe36c67819df0d
yaksin <yaksinvasquez@gmail.com>
2024-06-21 19:11:46 -0500

    add animation loading

--- js/logica_scroll_cont_skills.js
+++ js/logica_scroll_cont_skills.js
@@ -59,4 +59,526 @@ proyectosuno.to("#tresp", {
     duration: 100,
 });
 
+const load = gsap.timeline({
+    repeat: 0,
+    repeatDelay: 0,
+});
 
+load.to(".n01", {
+    color:"black",
+    duration: 0.5,
+});
+load.to(".n01", {
+    color:"#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n02", {
+    color:"black",
+    duration: 0.05,
+});
+load.to(".n02", {
+    color:"#ffffff00",
+    duration: 0.05,
+});
+
+load.to(".n03", {
+    color:"black",
+    duration: 0.01,
+});
+load.to(".n03", {
+    color:"#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n04", {
+    color:"black",
+    duration: 0.01,
+});
+load.to(".n04", {
+    color:"#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n05", {
+    color:"black",
+    duration: 0.01,
+});
+load.to(".n05", {
+    color:"#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n06", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n06", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n07", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n07", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n08", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n08", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n09", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n09", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+
+load.to(".n15", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n15", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n16", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n16", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n17", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n17", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n18", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n18", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n19", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n19", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n20", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n20", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n21", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n21", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n22", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n22", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n23", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n23", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n24", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n24", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+
+load.to(".n35", {
+    color: "black",
+    duration: 0.5,
+});
+load.to(".n35", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n36", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n36", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n37", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n37", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n38", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n38", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n39", {
+    color: "black",
+    duration: 0.6,
+});
+load.to(".n39", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n40", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n40", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n41", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n41", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n42", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n42", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n43", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n43", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n44", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n44", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n45", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n45", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n46", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n46", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n47", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n47", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n48", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n48", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n49", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n49", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n50", {
+    color: "black",
+    duration: 0.5,
+});
+load.to(".n50", {
+    color: "#ffffff00",
+    duration: 0.5,
+});
+
+load.to(".n51", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n51", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n52", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n52", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n53", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n53", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n54", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n54", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n55", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n55", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n56", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n56", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n57", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n57", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n58", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n58", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n67", {
+    color: "black",
+    duration: 0.8,
+});
+load.to(".n67", {
+    color: "#ffffff00",
+    duration: 0.1,
+});
+
+load.to(".n68", {
+    color: "black",
+    duration: 0.1,
+});
+load.to(".n68", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n69", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n69", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n70", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n70", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n76", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n76", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n77", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n77", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n82", {
+    color: "black",
+    duration: 0.5,
+});
+load.to(".n82", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n83", {
+    color: "black",
+    duration: 0.5,
+});
+load.to(".n83", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n84", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n84", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n85", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n85", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n86", {
+    color: "black",
+    duration: 0.01,
+});
+load.to(".n86", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n98", {
+    color: "black",
+    duration: 0.5,
+});
+load.to(".n98", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n99", {
+    color: "black",
+    duration: 0.5,
+});
+load.to(".n99", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".n100", {
+    color:"black"
+});
+load.to(".n100", {
+    color: "#ffffff00",
+    duration: 0.01,
+});
+
+load.to(".animacion_loading", {
+    background: "#ffffff00",
+    duration: 0.01,
+});
