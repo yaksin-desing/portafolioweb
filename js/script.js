953b7b9eb06839326e02952d9642a2fa71590693
yaksin <yaksinvasquez@gmail.com>
2024-07-23 10:13:17 -0500

    mod de script para arreglar problemas con el logo

--- js/script.js
+++ js/script.js
@@ -273,31 +273,6 @@ timedia.add("(max-width:800px)", () => {
 
 
 
-const proyectosuno = gsap.timeline({
-    scrollTrigger: {
-        trigger: "#cont_proyectos_scrolls",
-        start: 'top top',
-        end: '600% top',
-        markers: false,
-        pin: true,
-        scrub: 1,
-    }
-});
-
-proyectosuno.to("#unop", {
-    y: "0vh",
-    duration: 100,
-});
-
-proyectosuno.to("#dosp", {
-    y: "0vh",
-    duration: 100,
-});
-
-proyectosuno.to("#tresp", {
-    y: "0vh",
-    duration: 100,
-});
 
 
 
@@ -610,5 +585,30 @@ function menuactivo(entries) {
 
 
 
+const proyectosuno = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#cont_proyectos_scrolls",
+        start: 'top top',
+        end: '600% top',
+        markers: false,
+        pin: true,
+        scrub: 1,
+    }
+});
+
+proyectosuno.to("#unop", {
+    y: "0vh",
+    duration: 100,
+});
+
+proyectosuno.to("#dosp", {
+    y: "0vh",
+    duration: 100,
+});
+
+proyectosuno.to("#tresp", {
+    y: "0vh",
+    duration: 100,
+});
 
 
